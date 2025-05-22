f0369a72dd80b9692629c9b31d380b0e883c5424
Iaio99 <giuliano.vallone@ikmail.com>
2025-05-22 16:39:16 +0200

    Added netman to modify remote folders

--- lua/hackerinotopolino/plugins/neo-tree.lua
+++ lua/hackerinotopolino/plugins/neo-tree.lua
@@ -5,24 +5,46 @@ return{
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
+    "miversen33/netman.nvim"
   },
   lazy = false,
   config = function()
-      require("neo-tree").setup({
---        sources = {
---          "netman.ui.neo-tree"
---        },
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = true,
-            hide_gitignored = false,
-            always_show_by_pattern = {
-              ".git*",
-              ".env*",
-            },
+    require("neo-tree").setup({
+      sources = {
+        "filesystem",
+        "buffers",
+        "git_status",
+        "netman.ui.neo-tree"
+      },
+      source_selector = {
+        winbar = true,
+        status_line = false,
+        sources = {
+          {
+            source = "filesystem",                                -- string
+          },
+          {
+            source = "buffers",                                   -- string
+          },
+          {
+            source = "git_status",                                -- string
+          },
+          {
+            source = "remote"
           }
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = true,
+          hide_gitignored = false,
+          always_show_by_pattern = {
+            ".git*",
+            ".env*",
+          },
         }
+      }
       })
   end
 }
