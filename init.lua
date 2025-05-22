3ce83c0645323c4caf057ed05307dcc097a4fceb
Iaio99 <giuliano.vallone@ikmail.com>
2025-05-22 20:25:51 +0200

    Changed from VimEnter to UiEnter. Now it changes the directory when opening a file in an other directory

--- init.lua
+++ init.lua
@@ -3,8 +3,9 @@ vim.g.loaded_netrwPlugin = 1
 
 require("hackerinotopolino")
 
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
+vim.api.nvim_create_autocmd("UiEnter", {
+  callback = function(data)
+    vim.cmd("cd " .. vim.fn.fnamemodify(data.file, ":h"))
     vim.cmd("Neotree show")
   end
 })
