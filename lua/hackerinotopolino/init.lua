c9de209c3e23104e73e88ff962fa3a8c54193dde
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-05-28 15:11:44 +0200

    Changed theme

--- lua/hackerinotopolino/init.lua
+++ lua/hackerinotopolino/init.lua
@@ -2,4 +2,4 @@ require("hackerinotopolino.config.lazy")
 require("hackerinotopolino.config.mapping")
 require("hackerinotopolino.config.settings")
 
-vim.cmd [[colorscheme github_dark_default]]
+vim.cmd [[colorscheme nightfly]]
