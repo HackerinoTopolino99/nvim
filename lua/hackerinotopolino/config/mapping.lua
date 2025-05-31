ebc418018a6214a42803d90d82f99505dfc219d2
Iaio99 <giuliano.vallone@ikmail.com>
2025-05-31 02:41:54 +0200

    Removed custom keymaps for save, undo and redo

--- lua/hackerinotopolino/config/mapping.lua
+++ lua/hackerinotopolino/config/mapping.lua
@@ -1,10 +1,6 @@
--- Usefull keymaps
-vim.keymap.set({"n", "i"}, "<C-z>", "<cmd>u<CR>")
-vim.keymap.set({"n", "i"}, "<C-y>", "<cmd>redo<CR>")
-vim.keymap.set({"n", "i"}, "<C-s>", "<cmd>w<CR>")
-
 -- Navigation between tabs
 vim.keymap.set("n", "<C-right>", "<cmd>tabnext<CR>")
 vim.keymap.set("n", "<C-Left>", "<cmd>tabprevious<CR>")
 
 vim.keymap.set({"n","i"}, "<C-b>", "<cmd>Neotree toggle action=show<CR>")
+vim.keymap.set({"n", "i", "v"}, "<C-\\>", "<cmd>ToggleTerm<CR>")
