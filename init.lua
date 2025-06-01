2eb7cfa527798318493a84b6e325ac2ad4a1a6eb
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-01 08:15:44 +0200

    Moved linefly options and the autocmd and added the configurations for the lsp

--- init.lua
+++ init.lua
@@ -3,21 +3,3 @@ vim.g.loaded_netrwPlugin = 1
 
 require("hackerinotopolino")
 
-vim.api.nvim_create_autocmd("UiEnter", {
-  callback = function(data)
-    local path = vim.fn.fnamemodify(data.file, ":h")
-    if vim.fn.isdirectory(path) == 1 then
-      vim.cmd("cd " .. path)
-      vim.cmd("Neotree show")
-    end
-  end
-})
-
-
-vim.g.linefly_options = {
-  tabline = true,
-  with_file_icon = true,
-  with_git_branch = true,
-  with_git_status = false,
-  with_diagnostic_status = true,
-}
