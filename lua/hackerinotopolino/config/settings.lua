adfd985c0736680b575959d90545ff8a8abb44e1
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-03 20:34:57 +0200

    Refactoring, setted splitbelow=true and removed vim.opt.clipboard option

--- lua/hackerinotopolino/config/settings.lua
+++ lua/hackerinotopolino/config/settings.lua
@@ -1,20 +1,30 @@
-vim.opt.nu = true
+-- Misc options
+vim.opt.number = true
+vim.opt.mouse= "a"
+vim.opt.title = true
+
+-- Split options
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Indentation options
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
+
+-- Swapfile options
 vim.opt.swapfile = false
 vim.opt.backup = false
 
+-- Search navigation options
 vim.opt.hlsearch = true
 vim.opt.incsearch = false
 
+-- Aspect options
 vim.opt.termguicolors = true
 
-vim.opt.clipboard = "unnamedplus"
+-- Completion options
 vim.opt.completeopt = "noinsert,menuone,noselect"
-vim.opt.mouse= "a"
-vim.opt.splitright = true
-vim.opt.title = true
+
