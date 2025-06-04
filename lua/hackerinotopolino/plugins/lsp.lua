31712a6086086a1c0b7a8f2b3446775205dac2ed
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-04 18:49:43 +0200

    Added lua_ls configurations

--- lua/hackerinotopolino/plugins/lsp.lua
+++ lua/hackerinotopolino/plugins/lsp.lua
@@ -1,24 +1,35 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+  "neovim/nvim-lspconfig",
+  config = function()
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities()
+    )
 
-		local lspconfig = require("lspconfig")
+    local lspconfig = require("lspconfig")
 
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim", "it", "describe", "before_each", "after_each" },
+          },
+        },
+      },
+    })
 
     vim.diagnostic.config({
-			update_in_insert = true,
-			virtual_text = true,
-			signs = true,
-		})
-	end,
+      update_in_insert = true,
+      virtual_text = true,
+      signs = true,
+    })
+  end,
 }
