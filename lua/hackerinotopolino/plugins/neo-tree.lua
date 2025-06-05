4b9d8d4ce6fd2c6506699d5de406b8dfd87d0975
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-05 03:57:46 +0200

    Now the dotfiles are not showed except for .env, .gitignore and .git

--- lua/hackerinotopolino/plugins/neo-tree.lua
+++ lua/hackerinotopolino/plugins/neo-tree.lua
@@ -1,50 +1,53 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-		"miversen33/netman.nvim",
-	},
-	lazy = false,
-	config = function()
-		require("neo-tree").setup({
-			sources = {
-				"filesystem",
-				"buffers",
-				"git_status",
-				"netman.ui.neo-tree",
-			},
-			source_selector = {
-				winbar = true,
-				status_line = false,
-				sources = {
-					{
-						source = "filesystem", -- string
-					},
-					{
-						source = "buffers", -- string
-					},
-					{
-						source = "git_status", -- string
-					},
-					{
-						source = "remote",
-					},
-				},
-			},
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					hide_dotfiles = true,
-					hide_gitignored = false,
-					always_show_by_pattern = {
-						".git*",
-						".env*",
-					},
-				},
-			},
-		})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "miversen33/netman.nvim",
+  },
+  lazy = false,
+  config = function()
+    require("neo-tree").setup({
+      sources = {
+        "filesystem",
+        "buffers",
+        "git_status",
+        "netman.ui.neo-tree",
+      },
+      source_selector = {
+        winbar = true,
+        status_line = false,
+        sources = {
+          {
+            source = "filesystem", -- string
+          },
+          {
+            source = "buffers", -- string
+          },
+          {
+            source = "git_status", -- string
+          },
+          {
+            source = "remote",
+          },
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = true,
+          hide_gitignored = false,
+          always_show = {
+            ".git",
+            ".gitignore",
+          },
+          always_show_by_pattern = {
+            ".env*",
+          },
+        },
+      },
+    })
+  end,
 }
