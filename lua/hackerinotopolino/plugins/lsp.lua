e4df2f2643cc1dcb96d9a0e629c271dec453e4dc
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-06-05 19:08:47 +0200

    Rewritten configuration for the ansible lsp and terraform lsp

--- lua/hackerinotopolino/plugins/lsp.lua
+++ lua/hackerinotopolino/plugins/lsp.lua
@@ -1,61 +1,72 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local cmp_lsp = require("cmp_nvim_lsp")
-    local capabilities = vim.tbl_deep_extend(
-      "force",
-      {},
-      vim.lsp.protocol.make_client_capabilities(),
-      cmp_lsp.default_capabilities()
-    )
+	"neovim/nvim-lspconfig",
+	config = function()
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-    local lspconfig = require("lspconfig")
+		local lspconfig = require("lspconfig")
 
-    lspconfig.ansiblels.setup({
-      capabilities = capabilities,
-      settings = {
-        ansible = {
-          ansible = {
-            path = "ansible",
-          },
-          ansibleLint = {
-            path = "ansible-lint",
-          },
-        },
-      },
-    })
+		lspconfig.ansiblels.setup({
+			capabilities = capabilities,
+			cmd = { "ansible-language-server", "--stdio" },
+			settings = {
+				ansible = {
+					python = {
+						interpreterPath = "python3",
+					},
+					ansible = {
+						path = "ansible",
+					},
+					executionEnvironment = {
+						enabled = false,
+					},
+					validation = {
+						enabled = true,
+						ling = {
+							enabled = true,
+							path = "ansible-lint",
+						},
+					},
+				},
+			},
+			filetypes = { "yaml.ansible" },
+			root_dir = lspconfig.util.root_pattern("ansible.cfg", ".git"),
+			single_file_support = true,
+		})
 
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim", "it", "describe", "before_each", "after_each" },
-          },
-        },
-      },
-    })
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim", "it", "describe", "before_each", "after_each" },
+					},
+				},
+			},
+		})
 
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-    })
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+		})
 
-    lspconfig.terraformls.setup({
-      capabilities = capabilities,
-      settings = {
-        terraform = {
-          path = "terraform",
-        },
-        terraform_lsp = {
-          path = "terraform-lsp",
-        },
-      },
-    })
+		lspconfig.terraformls.setup({
+			capabilities = capabilities,
+			default_config = {
+				cmd = { "terraform-ls", "serve" },
+				filetypes = { "terraform", "terraform-vars" },
+				root_dir = lspconfig.util.root_pattern(".terraform", ".git"),
+			},
+		})
 
-    vim.diagnostic.config({
-      update_in_insert = true,
-      virtual_text = true,
-      signs = true,
-    })
-  end,
+		vim.diagnostic.config({
+			update_in_insert = true,
+			virtual_text = true,
+			signs = true,
+		})
+	end,
 }
