25db8194e118b49c6bd38893e0ac640ba8bbbe74
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-05 04:37:24 +0200

    Added git help and added some options

--- lua/hackerinotopolino/plugins/neo-tree.lua
+++ lua/hackerinotopolino/plugins/neo-tree.lua
@@ -10,6 +10,32 @@ return {
   lazy = false,
   config = function()
     require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "NC",
+      enable_diagnostics = false,
+      sort_case_insensitive = true,
+      default_component_configs = {
+        file_size = {
+          enabled = false,
+        },
+        type = {
+          enabled = false,
+        },
+        last_modified = {
+          enabled = false,
+        },
+        created = {
+          enabled = false,
+        },
+        symlink_target = {
+          true,
+        },
+      },
+      commands = {},
+      window = {
+        position = "left",
+        width = 40,
+      },
       sources = {
         "filesystem",
         "buffers",
@@ -48,36 +74,36 @@ return {
           },
         },
       },
-        git_status = {
-          window = {
-            position = "float",
-            mappings = {
-              ["g"] = {
-                "show_help",
-                nowait = false,
-                config = { title = "Git Commands", prefix_key = "g" },
-              },
-              ["gA"] = "git_add_all",
-              ["gu"] = "git_unstage_file",
-              ["ga"] = "git_add_file",
-              ["gr"] = "git_revert_file",
-              ["gc"] = "git_commit",
-              ["gp"] = "git_push",
-              ["gg"] = "git_commit_and_push",
-              ["o"] = {
-                "show_help",
-                nowait = false,
-                config = { title = "Order by", prefix_key = "o" },
-              },
-              ["oc"] = { "order_by_created", nowait = false },
-              ["od"] = { "order_by_diagnostics", nowait = false },
-              ["om"] = { "order_by_modified", nowait = false },
-              ["on"] = { "order_by_name", nowait = false },
-              ["os"] = { "order_by_size", nowait = false },
-              ["ot"] = { "order_by_type", nowait = false },
+      git_status = {
+        window = {
+          position = "float",
+          mappings = {
+            ["g"] = {
+              "show_help",
+              nowait = false,
+              config = { title = "Git Commands", prefix_key = "g" },
             },
+            ["gA"] = "git_add_all",
+            ["gu"] = "git_unstage_file",
+            ["ga"] = "git_add_file",
+            ["gr"] = "git_revert_file",
+            ["gc"] = "git_commit",
+            ["gp"] = "git_push",
+            ["gg"] = "git_commit_and_push",
+            ["o"] = {
+              "show_help",
+              nowait = false,
+              config = { title = "Order by", prefix_key = "o" },
+            },
+            ["oc"] = { "order_by_created", nowait = false },
+            ["od"] = { "order_by_diagnostics", nowait = false },
+            ["om"] = { "order_by_modified", nowait = false },
+            ["on"] = { "order_by_name", nowait = false },
+            ["os"] = { "order_by_size", nowait = false },
+            ["ot"] = { "order_by_type", nowait = false },
           },
         },
+      },
     })
   end,
 }
