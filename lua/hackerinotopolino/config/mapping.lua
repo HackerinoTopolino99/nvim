674b2fdb0a9e389da8e4a47bcadd16c05d48fc1c
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-05 04:36:43 +0200

    Oganized mappings and added mappings for interact with the clipboard system

--- lua/hackerinotopolino/config/mapping.lua
+++ lua/hackerinotopolino/config/mapping.lua
@@ -1,28 +1,35 @@
--- Navigation between tabs
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<C-right>", "<cmd>tabnext<CR>")
-vim.keymap.set("n", "<C-Left>", "<cmd>tabprevious<CR>")
+-- Navigation macros
+vim.keymap.set({ "n", "i", "v" }, "<C-right>", "<cmd>tabnext<CR>")
+vim.keymap.set({ "n", "i", "v" }, "<C-Left>", "<cmd>tabprevious<CR>")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
 
-vim.keymap.set({ "n", "i" }, "<C-b>", "<cmd>Neotree toggle action=show<CR>")
-vim.keymap.set({ "n", "i", "v" }, "<C-\\>", "<cmd>ToggleTerm<CR>")
+-- System clipboard interaction
+vim.keymap.set("n", "<leader>p", [["+p]])
+vim.keymap.set("n", "<leader>P", [["+P]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- NeoTree Mappings
+vim.keymap.set({ "n", "v", "i" }, "<C-b>", "<cmd>Neotree toggle action=show<CR>")
+vim.keymap.set({ "n" }, "<leader>e", "<cmd>NeoTree reveal<CR>")
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- ToggleTerm Mappings
+vim.keymap.set({ "n", "i", "v" }, "<leader>t", "<cmd>ToggleTerm<CR>")
 
+-- LSP Mappings
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- Usefull keybindings
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
