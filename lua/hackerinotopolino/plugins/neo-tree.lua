965d7c272826684d02167ce861e444ef3029c7dc
Iaio99 <giuliano.vallone@ikmail.com>
2025-06-05 04:01:28 +0200

    Added help menu for git commands

--- lua/hackerinotopolino/plugins/neo-tree.lua
+++ lua/hackerinotopolino/plugins/neo-tree.lua
@@ -48,6 +48,36 @@ return {
           },
         },
       },
+        git_status = {
+          window = {
+            position = "float",
+            mappings = {
+              ["g"] = {
+                "show_help",
+                nowait = false,
+                config = { title = "Git Commands", prefix_key = "g" },
+              },
+              ["gA"] = "git_add_all",
+              ["gu"] = "git_unstage_file",
+              ["ga"] = "git_add_file",
+              ["gr"] = "git_revert_file",
+              ["gc"] = "git_commit",
+              ["gp"] = "git_push",
+              ["gg"] = "git_commit_and_push",
+              ["o"] = {
+                "show_help",
+                nowait = false,
+                config = { title = "Order by", prefix_key = "o" },
+              },
+              ["oc"] = { "order_by_created", nowait = false },
+              ["od"] = { "order_by_diagnostics", nowait = false },
+              ["om"] = { "order_by_modified", nowait = false },
+              ["on"] = { "order_by_name", nowait = false },
+              ["os"] = { "order_by_size", nowait = false },
+              ["ot"] = { "order_by_type", nowait = false },
+            },
+          },
+        },
     })
   end,
 }
