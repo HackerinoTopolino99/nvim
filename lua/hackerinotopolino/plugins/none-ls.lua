bee396dce33d9e1a42454dac769d0a54cee4dbdc
Iaio99 <giuliano.vallone@ikmail.com>
2025-08-24 17:51:25 +0200

    Added formatters and linters for opentofu, bash, hcl and actionlint

--- lua/hackerinotopolino/plugins/none-ls.lua
+++ lua/hackerinotopolino/plugins/none-ls.lua
@@ -1,19 +1,23 @@
 return {
-	"nvimtools/none-ls.nvim",
+  "nvimtools/none-ls.nvim",
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
-    },
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
---				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.ansiblelint,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.packer,
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.diagnostics.actionlint,
+        null_ls.builtins.diagnostics.ansiblelint,
+        null_ls.builtins.diagnostics.opentofu_validate,
+        null_ls.builtins.formatting.hclfmt,
+        null_ls.builtins.formatting.opentofu_fmt,
+        null_ls.builtins.formatting.packer,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.stylua,
         require("none-ls.formatting.autopep8"),
         require("none-ls.diagnostics.flake8"),
-			},
-		})
-	end,
+      },
+    })
+  end,
 }
