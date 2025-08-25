6b35d1057c5c94b6abde28e95d3fb5ceb81d4000
HackerinoTopolino <giuliano.vallone@ikmail.com>
2025-08-25 12:42:17 +0200

    Corrette configurazioni per ansiblels e aggiunte configurazioni per gli altri lsp

--- lua/hackerinotopolino/plugins/lsp.lua
+++ lua/hackerinotopolino/plugins/lsp.lua
@@ -1,66 +1,84 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local cmp_lsp = require("cmp_nvim_lsp")
-		local capabilities = vim.tbl_deep_extend(
-			"force",
-			{},
-			vim.lsp.protocol.make_client_capabilities(),
-			cmp_lsp.default_capabilities()
-		)
+  "neovim/nvim-lspconfig",
+  config = function()
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+      "force",
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities()
+    )
 
-		local lspconfig = require("lspconfig")
+    local lspconfig = require("lspconfig")
 
-		lspconfig.ansiblels.setup({
-			capabilities = capabilities,
-			cmd = { "ansible-language-server", "--stdio" },
-			settings = {
-				ansible = {
-					ansible = {
-						path = "ansible",
-					},
-					executionEnvironment = {
-						enabled = false,
-					},
-					validation = {
-						enabled = true,
-						lint = {
-							enabled = true,
-							path = "ansible-lint",
-						},
-					},
-          completion = {
-            provideRedirectModules = true,
-            provideModuleOptionAliases = true
-          },
-				},
-			},
-			filetypes = { "yaml.ansible", "yaml", "yml" },
-			root_dir = lspconfig.util.root_pattern("ansible.cfg", ".git"),
-			single_file_support = true,
-		})
+    lspconfig.ansiblels.setup({
+      capabilities = capabilities,
+      filetypes = { "yaml" },
+      root_dir = lspconfig.util.root_pattern(
+        "ansible.cfg",
+        "inventory",
+        "inventory.yml",
+        "inventory.yaml",
+        "inventory.ini",
+        "inventory.example"
+      ),
+      single_file_support = false,
+    })
 
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.bashls.setup({
+      capabilities = capabilities,
+    })
 
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-		})
+    lspconfig.docker_language_server.setup({
+      capabilities = capabilities,
+    })
 
-		lspconfig.terraformls.setup({
-			capabilities = capabilities,
-			default_config = {
-				cmd = { "terraform-ls", "serve" },
-				filetypes = { "terraform", "terraform-vars" },
-				root_dir = lspconfig.util.root_pattern(".terraform", ".git"),
-			},
-		})
+    lspconfig.gitlab_ci_ls.setup({
+      capabilities = capabilities,
+    })
 
-		vim.diagnostic.config({
-			update_in_insert = true,
-			virtual_text = true,
-			signs = true,
-		})
-	end,
+    lspconfig.gh_actions_ls.setup({
+      capabilities = capabilities,
+
+      filetypes = { "yaml.github" },
+      root_dir = lspconfig.util.root_pattern(".github/workflows"),
+      single_file_support = false,
+    })
+
+    lspconfig.ltex_plus.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.nginx_language_server.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.systemd_ls.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.tofu_ls.setup({
+      capabilities = capabilities,
+    })
+
+    lspconfig.yamlls.setup({
+      capabilities = capabilities,
+      filetypes = { "yaml" },
+      single_file_support = true,
+    })
+
+    vim.diagnostic.config({
+      update_in_insert = true,
+      virtual_text = true,
+      signs = true,
+    })
+  end,
 }
